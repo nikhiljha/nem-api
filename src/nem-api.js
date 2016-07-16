f4c3893d8aae1cc1759bcb6579d86701fc850e5a
nikhiljha <nikhilfromirvine@gmail.com>
2016-07-15 17:33:13 -0700

    Finished adding all methods
    Thanks sheld0r

--- src/nem-api.js
+++ src/nem-api.js
@@ -79,6 +79,128 @@ module.exports = class nisapi extends EventEmitter {
     return this._request('account/importances', {});
   }
 
+  account_get_namespace_page(address, parent, id, pageSize) {
+    return this._request('/account/namespace/page', { qs: { address: address, parent: parent, id: id, pagesize: pagesize } });
+  }
+
+  account_get_mosaic_definition_page(address, parent, id) {
+    return this._request('/account/mosaic/definition/page', { qs: { address: address, parent: parent, id: id } });
+  }
+
+  account_get_mosaic_owned(address) {
+    return this._request('/account/mosaic/owned', { qs: { address: address } });
+  }
+
+  account_post_unlock(privateKey) {
+    return this._request('/account/unlock', { qs: { privateKey: privateKey } });
+  }
+
+  account_post_lock(privateKey) {
+    return this._request('/account/lock', { qs: { privateKey: privateKey } });
+  }
+
+  account_post_unlocked_info() {
+    return this._request('/account/unlocked/info', {});
+  }
+
+  account_get_historical_get(address, startHeight, endHeight, increment) {
+    return this._request('/account/historical/get', { qs: { address: address, startHeight: startHeight, endHeight: endHeight, increment: increment } });
+  }
+
+  chain_get_height() {
+    return this._request('/chain/height', {});
+  }
+
+  chain_get_score() {
+    return this._request('/chain/score', {});
+  }
+
+  chain_get_last_block() {
+    return this._request('/chain/last-block', {});
+  }
+
+  block_get_get(blockHash) {
+    return this._request('/block/get', { qs: { blockHash: blockHash } });
+  }
+
+  block_post_at_public(blockHeight) {
+    return this._request('/block/at/public', { qs: { blockHeight: blockHeight } });
+  }
+
+  local_post_chain_blocks_after(blockHeight) {
+    return this._request('/local/chain/blocks-after', { qs: { blockHeight: blockHeight } });
+  }
+
+  node_get_info() {
+    return this._request('/node/info', {});
+  }
+
+  node_get_extended_info() {
+    return this._request('/node/extended-info', {});
+  }
+
+  node_get_peer_list_all() {
+    return this._request('/node/peer-list/all', {});
+  }
+
+  node_get_peer_list_reachable() {
+    return this._request('/node/peer-list/reachable', {});
+  }
+
+  node_get_peer_list_active() {
+    return this._request('/node/peer-list/active', {});
+  }
+
+  node_get_active_peers_max_chain_height() {
+    return this._request('/node/active-peers/max-chain-height', {});
+  }
+
+  node_get_experiences() {
+    return this._request('/node/experiences', {});
+  }
+
+  node_post_boot(bootNodeRequest ) {
+    return this._request('/node/boot', { qs: { bootNodeRequest: bootNodeRequest } });
+  }
+
+  namespace_get_root_page(id, pagesize) {
+    return this._request('/namespace/root/page', { qs: { id: id, pagesize: pagesize } });
+  }
+
+  namespace_get(namespace) {
+    return this._request('/namespace', { qs: { namespace: namespace } });
+  }
+
+  namespace_get_mosaic_definition_page(namespace, id, pagesize) {
+    return this._request('/namespace/mosaic/definition/page', { qs: { namespace: namespace, id: id, pagesize: pagesize } });
+  }
+
+  transaction_post_prepare_announce(requestPrepareAnnounce) {
+    return this._request('/transaction/prepare-announce', { qs: { requestPrepareAnnounce: requestPrepareAnnounce } });
+  }
+
+  transaction_post_announce(requestAnnounce) {
+    return this._request('/transaction/announce', { qs: { requestAnnounce: requestAnnounce } });
+  }
+
+  debug_get_time_synchronization() {
+    return this._request('/debug/time-synchronization', {});
+  }
+
+  debug_get_connections_incoming() {
+    return this._request('/debug/connections/incoming', {});
+  }
+
+  debug_get_connections_outgoing() {
+    return this._request('/debug/connections/outgoing', {});
+  }
+
+  debug_get_connections_timers() {
+    return this._request('/debug/connections/timers', {});
+  }
+
+
+
   _request(_path, options = {}) {
     if (!this.endpoint) {
       throw new Error('NIS endpoint not provided!');
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var nodeis = require('./index.js');
 const Promise = require('bluebird');
-var meme = new nisapi('bob.nem.ninja:7890');
+var meme = new nodeis('bob.nem.ninja:7890');
 
 meme.account_get('TALICELCD3XPH4FFI5STGGNSNSWPOTG5E4DS2TOS').then(function(response) {
   console.log(response);
