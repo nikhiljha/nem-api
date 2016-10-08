dfe030e1a800be159ca6a6b801bead9efbc4716f
Nikhil Jha <nikhilfromirvine@gmail.com>
2016-10-08 12:06:33 -0700

    Add docs.

--- src/nem-api.js
+++ src/nem-api.js
@@ -2,12 +2,16 @@
 
 const Promise = require('bluebird');
 const unirest = require('unirest');
+const stompit = require('stompit');
 
 module.exports = class nisapi {
 
-  // One object per NIS used, this makes the frontend code cleaner.
+  /**
+   * constructor - Give the object an NIS URL to interact with.
+   *
+   * @param {string} endpoint HTTP formatted base URL of the NIS, including port.
+   */
   constructor(endpoint) {
-    // Make sure the endpoint isn't null and log a warning if there is.
     if (endpoint) {
       // May be unclear to newer developers so I'll explain it here.
       // Slicing -1 starts from the end of the string.
@@ -25,44 +29,100 @@ module.exports = class nisapi {
     }
   }
 
-  // Weird workaround to convert JSON to URL Parameters.
-  getAsUriParameters(data) {
+  /**
+   * asuri - Given JSON, convert to URL syntax.
+   *
+   * @param  {string} data JSON formatted data
+   * @return {string}      URI formatted arguments
+   */
+  asuri(data) {
    var url = '';
    for (var prop in data) {
       url += encodeURIComponent(prop) + '=' +
-          encodeURIComponent(data[prop]) + '&';
+             encodeURIComponent(data[prop]) + '&';
    }
    return "?" + url.substring(0, url.length - 1)
  }
 
-  // All 'get' requests are handled here.
+
+  /**
+   * get - Sends an HTTP GET request to the API.
+   *
+   * @param  {string}   path        Relative path of API call.
+   * @param  {json}     data        JSON formatted object to send.
+   * @param  {function} callback    Callback that is given a response object.
+   */
   get(path, data, callback) {
-    unirest.get(this.endpoint + path + this.getAsUriParameters(data))
+    unirest.get(this.endpoint + path + this.asuri(data))
     .headers({'Accept':'application/json', 'Content-Type':'application/json'})
     .end(callback);
   }
 
-  // All 'post' requests are handled here.
+
+  /**
+   * post - Sends an HTTP Post request to the API.
+   *
+   * @param  {string}   path        Relative path of API call.
+   * @param  {json}     data        JSON formatted object to send.
+   * @param  {function} callback    Callback that is given a response object.
+   */
   post(path, data, callback) {
-    unirest.post(this.endpoint + path + this.getAsUriParameters(data))
+    unirest.post(this.endpoint + path)
     .headers({'Accept':'application/json', 'Content-Type':'application/json'})
+    .send(data)
     .end(callback);
   }
 
-  // Converts the private key from hex into a useable format.
-  fixPrivateKey(privatekey) {
+
+  /**
+   * fixpkey - Fix a hex key for use in signing.
+   *
+   * @param  {string} privatekey Private key in hex format.
+   * @return {string}            Fixed private key.
+   */
+  fixpkey(privatekey) {
       return ("0000000000000000000000000000000000000000000000000000000000000000"
       + privatekey.replace(/^00/, '')).slice(-64);
   }
 
-  // Used for signing anything.
+
+  /**
+   * sign - Sign a given object with a given private key.
+   *
+   * @param  {string} privatekey Private key of NEM account in hex.
+   * @param  {object} content    Thing that you want to sign.
+   * @return {string}            Signature of object as a string.
+   */
   sign(privatekey, content) {
-    var KeyPaire = require('./signing/KeyPair.js');
-    var KeyPair = new KeyPaire();
-    var kp = KeyPair.create(this.fixPrivateKey(privatekey));
+    var KeyPairLibrary = require('./signing/KeyPair.js');
+    var KeyPair = new KeyPairLibrary();
+    var kp = KeyPair.create(this.fixpkey(privatekey));
     var signature = kp.sign(content).toString();
     return signature;
   }
 
+
+  /**
+   * calcFee - Calculate the minimum fee of a transaction.
+   *
+   * @param  {string} type       Type of transaction (transfer, multisig, etc).
+   * @param  {object} options    Options object which contains more information about the transaction.
+   * @return {integer}           The fee in XEM that needs to be paid.
+   */
+  calcFee(type, options) {
+    // TODO: Finish this.
+  }
+
+
+  /**
+   * signTransaction - description
+   *
+   * @param  {string} privatekey Private key in hex format.
+   * @param  {object} options    Object with all the transaction options.
+   * @return {object}            Object that contains the signature and rawbytes.
+   */
+  signTransaction(privatekey, options) {
+
+  }
 
 }
--- test.js
+++
@@ -1,6 +0,0 @@
-var nemapi = require('./index.js');
-var bob = new nemapi('http://san.nem.ninja:7890');
-
-bob.get('/account/generate', null, function(response) {
-  console.log(response.body.address);
-});
